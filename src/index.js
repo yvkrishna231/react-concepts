2bcdbf249e4d3a096520c775902bc9fe77234fab
kesava07 <kesava07.ip@gmail.com>
2019-09-25 04:49:24 -0700

    bootstrap including

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './ErrorHandling/App';
+// import App from './ErrorHandling/App';
 import * as serviceWorker from './serviceWorker';
 import "bootstrap/dist/css/bootstrap.min.css";
+// import HocApp from './HigherOrderComponent/HocApp';
+import ContextApp from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ContextApp />, document.getElementById('root'));
 
 serviceWorker.unregister();
